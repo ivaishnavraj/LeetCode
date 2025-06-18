cde7e6ddd53d86e09c294a95f8a6b7693d562427
Vaishnav Raj <132328507+ivaishnavraj@users.noreply.github.com>
2025-06-18 21:00:10 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (56.52%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -1,14 +1,15 @@
-//Approach: Use Digital root formula {result is always 1 + (num - 1) % 9}
+//Approach 2: If Digit is Single Just Return else do normal digit seperation and add that
 //Time Complexity: O(1)
 //Space Complexity: O(1)
 
 class Solution {
     public int addDigits(int num) {
-        if (num <= 0) return 0;
-        // Digital root formula:
-        return 1+(num-1)%9;       
-
-        // For any positive integer, the repeated digit sum is: 1 + (num - 1) % 9
-        // This gives the same result as adding digits repeatedly until one digit remains
-
-}}
+        while(num>9)
+        {
+            int rem = num%10;
+            int fnum = num/10;
+            num = fnum + rem;
+        }
+        return num;
+    }
+}
